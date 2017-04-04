5a1f14edbb17c6a1ea81e1837f84733e2b25ec58
Anthony Mirabeau <funkypurplemonkey@yahoo.ca>
2017-04-04 18:57:20 -0400

    River generation
    Incorporated rain map into map generation, so that generated terrain is
    affected by both height and quantity of precipitations. Added river
    generation.

--- Assets/Scripts/AutoMap.cs
+++ Assets/Scripts/AutoMap.cs
@@ -16,6 +16,8 @@ public class AutoMap : MonoBehaviour
 	private NoiseGenerator heightMap;
 	private NoiseGenerator rainMap;
 
+	private float[,] heightTable;
+
 	// Use this for initialization
 	void Start () 
 	{
@@ -25,6 +27,10 @@ public class AutoMap : MonoBehaviour
 
 		heightMap = new NoiseGenerator (1);
 		rainMap = new NoiseGenerator (1);
+		heightTable = new float[mapSize, mapSize];
+
+		//Candidates for locations to start rivers
+		List<Position2D> riverStartCandidates = new List<Position2D>();
 
 		for (int i = 0; i < mapSize; i++) 
 		{
@@ -43,33 +49,116 @@ public class AutoMap : MonoBehaviour
 				float height = heightMap.GetNoise (tileX, tileY) - heightDrop;
 				float rain = rainMap.GetNoise (tileX, tileY);
 
+				heightTable [i, j] = height;
+
 				//Set tiles on border of map to water tiles
 				if (i == 0 || j == 0 || i == mapSize - 1 || j == mapSize - 1) {
 					tile = new MapTile (Instantiate (water), Resource.Nothing, TileType.Water);
 				}
-				else if (height < 0) {
+				else if (height <= 0) {
 					tile = new MapTile (Instantiate (water), Resource.Nothing, TileType.Water);
 				} 
 				else if (height < 0.1f) {
 					tile = new MapTile (Instantiate (sand), Resource.Nothing, TileType.Sand);
 				} 
-				else if (height < 0.4f) {
-					tile = new MapTile (Instantiate (plains), Resource.Nothing, TileType.Plains);
+				else if (height < 0.4f) 
+				{
+					if (rain < 0.6f)
+						tile = new MapTile (Instantiate (plains), Resource.Nothing, TileType.Plains);
+					else
+						tile = new MapTile (Instantiate (forest), Resource.Nothing, TileType.Forest);
 				}
 				else {
 					tile = new MapTile (Instantiate (mountain), Resource.Nothing, TileType.Mountain);
+					if (rain >= 0.6f)
+						riverStartCandidates.Add (new Position2D (i, j));
 				} 
-
-					
-				
 
 				map.setTileAt (i,j, tile);
 			}
 		}
+
+
+		//Randomly pick river start locations. After a location is picked, eliminate other candidates that are too close to it
+		List<Position2D> riverStartLocations = new List<Position2D>();
+		int minRiverDistance = mapSize / 8;
+		while (riverStartCandidates.Count > 0) 
+		{
+			int next = Random.Range (0, riverStartCandidates.Count);
+			Position2D nextPos = riverStartCandidates [next];
+
+			bool tooClose = false;
+			//Check if too close to an already selected river tile
+			foreach (Position2D riverPos in riverStartLocations) 
+			{
+				//Check distance in manhattan distance
+				if (Mathf.Abs (nextPos.x - riverPos.x) + Mathf.Abs (nextPos.y - riverPos.y) < minRiverDistance) 
+				{
+					tooClose = true;
+					break;
+				}
+			}
+
+			if(!tooClose)
+				riverStartLocations.Add(nextPos);
+			
+			riverStartCandidates.RemoveAt(next);
+		}
+
+		//Dig rivers
+		foreach (Position2D riverPos in riverStartLocations) 
+		{
+			List<Position2D> river = new List<Position2D>();
+			Position2D currentPos = riverPos;
+			while (map.getTileTypeAt (currentPos.x, currentPos.y) != TileType.Water) 
+			{
+				river.Add (currentPos);
+				List<Position2D> neighbors = getRiverNeighbors (currentPos, river);
+
+				float minHeight = Mathf.Infinity;
+
+				if (neighbors.Count == 0)
+					break;
+				
+				Position2D minPos = new Position2D();
+				foreach (Position2D n in neighbors) 
+				{
+					float newHeight = heightTable [n.x, n.y];
+					if (newHeight < minHeight) {
+						minHeight = newHeight;
+						minPos = n;
+					}
+				}
+				currentPos = minPos;
+			}
+
+			foreach (Position2D nextRiv in river) 
+			{
+				heightTable[nextRiv.x, nextRiv.y] = 0;
+				map.setTileAt(nextRiv.x, nextRiv.y, new MapTile(Instantiate(water), Resource.Nothing, TileType.Water));
+			}
+		} 
 	}
 
-	// Update is called once per frame
-	void Update () {
+	//Returns a list of possible locations the river could go from a given tile
+	private List<Position2D> getRiverNeighbors(Position2D current, List<Position2D> river)
+	{
+		Position2D upPosition = new Position2D(current.x, current.y + 1);
+		Position2D downPosition = new Position2D(current.x, current.y - 1);
+		Position2D rightPosition = new Position2D(current.x + 1, current.y);
+		Position2D leftPosition = new Position2D(current.x - 1, current.y);
 
+		List<Position2D> neighbors = new List<Position2D> ();
+
+		if (upPosition.y < mapSize && !river.Contains (upPosition))
+			neighbors.Add (upPosition);
+		if (downPosition.y >=0 && !river.Contains (downPosition))
+			neighbors.Add (downPosition);
+		if (rightPosition.x < mapSize && !river.Contains (rightPosition))
+			neighbors.Add (rightPosition);
+		if (leftPosition.x >= 0 && !river.Contains (leftPosition))
+			neighbors.Add (leftPosition);
+		
+		return neighbors;
 	}
 }
---
+++ Assets/Scripts/Position2D.cs
@@ -0,0 +1,37 @@
+﻿public struct Position2D
+{
+	public int x;
+	public int y;
+
+	public Position2D(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+
+	public override bool Equals (object obj)
+	{
+		if (obj is Position2D) 
+		{
+			Position2D item = (Position2D)obj;
+			return x == item.x && y == item.y;
+		} else {
+			return false;
+		}
+	}
+
+	public static bool operator ==(Position2D p1, Position2D p2) 
+	{
+		return p1.Equals(p2);
+	}
+
+	public static bool operator !=(Position2D p1, Position2D p2) 
+	{
+		return !p1.Equals(p2);
+	}
+
+	//Written in to suppress warnings. These objects are never used as keys for dictionaries
+	public override int GetHashCode ()
+	{
+		return (x * y).GetHashCode ();
+	}
+}
