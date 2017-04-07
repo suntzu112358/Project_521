56481d996f621f5ec108fd5fcf1fdb051ff36f3f
Faraz Oman <faraz@farazoman.com>
2017-04-07 06:22:21 -0400

    added some comment notes to crafting recipie
    I"m tired... :(

--- Assets/Scripts/CraftingRecipe.cs
+++ Assets/Scripts/CraftingRecipe.cs
@@ -6,4 +6,8 @@ public class CraftingRecipe {
 
 	private Dictionary<Resource, int> inputs;
 	private Dictionary<Resource, int> outputs; //output tools as an output as well
+
+    //TODO create a factory to auto load and dynamically create the objects which should be stored as actions or something
+    //I need to sleep and think about this more
+    //auto load from json or something, maybe yaml? something simple
 }
