928ee3bf90bfb76ecf8638ac0c019aff1b827585
Faraz Oman <faraz@farazoman.com>
2017-04-07 06:19:41 -0400

    added base class as world info

---
+++ Assets/Scripts/Base.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//NOTE this class will function as a singleton
+class Base
+{
+    Inventory worldState;
+    Knowledge worldInfo;
+
+    Base instance = null;
+
+    private Base(Map map)
+    {
+        worldState = new Inventory(false);
+        worldInfo = new Knowledge(map);
+
+    }
+
+    public Base getInstance(Map map)
+    {
+        if(instance == null)
+        {
+            return new Base(map);
+        }
+        return instance;
+    }
+}
+
